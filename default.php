9b6d9118c302cdb52c87580ead7e21ea7298b097
Ryan King <161281690+Ryan-King-Dev@users.noreply.github.com>
2024-04-06 09:21:03 -0700

    update

--- default.php
+++ default.php
@@ -1,23 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ghostie Gear Incorporated</title>
-    <?php include ("styles.php"); ?>
+    <?php include("styles.php"); ?>
 
 </head>
-<body>
-    
-    <?php 
-        include ("connectDB.php");
-        include ("logo.php");
-        include ("cart.php");
-        include ("navbar.php");
+
+<body class='bodybg'>
+
+    <?php
+    include("connectDB.php");
+    include("logo.php");
+    include("cart.php");
+    include("navbar.php");
     ?>
-    
+    <h1>TEST</h1>
     <br>
     <a href="cart.html">CART</a>
     <br>
+
+    <!-- <? include("bootstrapjs.php"); ?> -->
 </body>
+
 </html>
